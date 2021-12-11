bdc26448a8ad035253650df36325bba73da794c2
Devin Kott <devin.kott@gmail.com>
2021-12-10 19:13:49 -0700

    updates

--- src/App.js
+++ src/App.js
@@ -3,15 +3,18 @@ import SectionHeader from "./SectionHeader";
 function App() {
     return (
         <main
-            className="container mx-auto font-mono text-md"
+            className="container mx-auto font-mono text-md subpixel-antialiased leading-7 text-neutral-800"
         >
             <section>
                 <SectionHeader title={`I'm Devin Kott, a computer scientist from Rochester, New York`}/>
                 <p>
-                    I recently graduated from the Rochester Institute of Technology with both a B.S. and M.S. in Computer Science, focusing on security (May 2021).
+                    I graduated from the Rochester Institute of Technology with both a B.S. and M.S. in Computer Science, focusing on security, in May 2021.
                     My goal in my career is to get started learning about modern technologies that cross with my interests: sustainability, anything space related, and automation systems to name a few.
                     I find all of these topics fascinating and would love to contribute to either new or existing solutions.
                 </p>
+            </section>
+            <section>
+                <SectionHeader title={`Education`}/>
             </section>
             <section>
                 <SectionHeader title={`Experience`}/>
@@ -26,11 +29,18 @@ function App() {
                 </p>
             </section>
             <section>
+                <SectionHeader title={`Education`}/>
+                <p>
+                    Test stuff.
+                </p>
+            </section>
+            <section>
                 <SectionHeader title={`More about me`}/>
                 <p>
-                    I enjoy messing around with home automation, fixing iPhones, rock climbing, playing volleyball, riding motorcycles, flying drones, photography, and learning French.
+                    I enjoy messing around with home automation, fixing iPhones, rock climbing, skiing, playing volleyball, riding motorcycles, flying drones, and photography.
                     Here are some of the technologies and hardware I use: Fedora, Ubuntu, Windows, NUC10i5, Synology DS218, Netgear Orbi, proxmox, Xiaomi, PiHole, TickTick.
-                    I use (or have used) a few AWS services as well: Amplify, Route 53, S3. My NUC10i5 runs proxmox with several containers for various services, including HomeAssistant, Plex, bitwarden, and caddy.
+                    I use (or have used) a few AWS services as well: Amplify, Route 53, S3.
+                    My NUC10i5 runs proxmox with several containers for various services, including HomeAssistant, Plex, bitwarden, and caddy.
                     Feel free to contact me by email at i @ a, where i = devin and a = devinkott.com. You can visit my linkedin at 'https://www.linkedin.com/in/' + i + '-kott'. You can visit my github at 'https://github.com/' + i + 'kott'.
                 </p>
             </section>
--- src/SectionHeader.js
+++ src/SectionHeader.js
@@ -2,7 +2,7 @@ function SectionHeader(props) {
     const { title } = props;
 
     return (
-        <h2 className="text-2xl pt-5 pb-5" >
+        <h2 className="text-2xl pt-5 pb-5 underline decoration-slate-600 text-neutral-900 hover:animate-pulse">
             {title}
         </h2>
     );
